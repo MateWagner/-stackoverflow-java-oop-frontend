1f8b37068d9a3d357a4aa353c5ff071c3d3dc726
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-03-30 01:00:28 +0200

    Add dateConverter, answerCard.css

--- src/answer/AnswerCard.js
+++ src/answer/AnswerCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
-function AnswerCard(desc) {
+import './answerCard.css'
+import useFetch from '../api/useFetch';
+function AnswerCard({ desc, clientId, date }) {
+  const { isPending, data, error } = useFetch('/client/' + clientId);
   return (
-    <div>
-      <h4>{desc}</h4>
+    <div className="answer-card">
+      <div className="answer-desc">{desc}</div>
+      <div className="answer-details">
+        <div className="answer-giver">{error ? "error" : (isPending ? "?" : data.name)}</div>
+        <div className="answer-date">{date}</div>
+      </div>
     </div>
   )
 }
--- src/answer/AnswerList.js
+++ src/answer/AnswerList.js
@@ -1,16 +1,16 @@
 import useFetch from "../api/useFetch"
+import dateConverter from "../util/dateConverter";
 import AnswerCard from "./AnswerCard";
 
 const AnswerList = ({questionId}) => {
   const { isPending, data, error } = useFetch('/answers/answers_of_question/' + questionId)
-  console.log(questionId)
   return (
     <div>
       <h1>Answers</h1>
       {isPending && <p>Loading...</p>}
       {error && <h1> {error} </h1>}
       {data &&
-        data.map((answer) => <AnswerCard key={answer.id} desc={answer.description} />)}
+        data.map((answer) => <AnswerCard key={answer.id} desc={answer.description}  date={dateConverter(answer.date)} clientId={answer.client_id} /> )}
     </div>
   );
 }
