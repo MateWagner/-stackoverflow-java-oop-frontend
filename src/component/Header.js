441c3396f5638a20ffc88bc8ce95246c6319352b
MateWagner <wwagnermate@gmail.com>
2023-03-29 11:13:58 +0200

    feat: link to add new question

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,10 @@
 import './header.css'
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <nav>
       <div><h1>Full Stack Overflow</h1></div>
+      <div><Link to={'/new/question'}>Add New Question</Link></div>
     </nav>
   );
 }
