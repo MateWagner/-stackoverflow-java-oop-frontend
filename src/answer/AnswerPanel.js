f103917f4e2659c0b283a6948b213ebf7f71d22d
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-03-30 01:46:20 +0200

    Add TODO-s to mark future tasks

--- src/answer/AnswerPanel.js
+++ src/answer/AnswerPanel.js
@@ -6,7 +6,14 @@ const AnswerPanel = () => {
   const { postAsync } = useFetchPost();
   const [answerContent, setAnswerContent] = useState('');
   const contentChange = event => { setAnswerContent(event.target.value); }
+  //TODO: Replace hard-coded values and make changes on site without reloading the page.
+  /*  
+      Change the hard coded parameters (questionId & clientId) of new answer
+      posting when login already working and we can get the ID of current client.
 
+      Use the returned ID of the newly posted answer to extend the AnswerList, so
+      we don't have to reload the site for seeing changes.
+  */
   const postNewAnswer = async () => {
     const url = 'answers';
     const answerObject = {
@@ -14,8 +21,8 @@ const AnswerPanel = () => {
       'questionId': 1,
       'clientId': 1
     }
-    const a = await postAsync(url, answerObject);
-    console.log(a);
+    const returnedIdOfAnswer = await postAsync(url, answerObject);
+    console.log(returnedIdOfAnswer);
   }
   return (
     <div className='answerPanel'>
