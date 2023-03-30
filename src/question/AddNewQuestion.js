e1317ef7054ac048a3f00bb7c6ca6dd40cc50cfa
MateWagner <wwagnermate@gmail.com>
2023-03-30 18:35:03 +0200

    fix: remove unessesary code

--- src/question/AddNewQuestion.js
+++ src/question/AddNewQuestion.js
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../UserContext";
 
 const AddNewQuestion = () => {
