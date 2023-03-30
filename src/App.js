17a6b4d9276c9e50d70fcb488496eaa7b598eb3b
MateWagner <wwagnermate@gmail.com>
2023-03-30 11:32:49 +0200

    user context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Header from './component/Header';
 import AddNewQuestion from './question/AddNewQuestion';
 import Question from './question/Question';
 import QuestionList from './question/QuestionList';
+import { UserContext } from './UserContext'
 
 function App() {
+  const [user, setUser] = useState(null)
   return (
     <div className="App">
-      <Header />
-      <div className='content'>
-        <Routes>
-          <Route path='/' element={<QuestionList />} />
-          <Route path='/new/question' element={<AddNewQuestion />} />
-          <Route path='question/:id' element={<Question />} />
-        </Routes>
-      </div>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Header />
+        <div className='content'>
+          <Routes>
+            <Route path='/' element={<QuestionList />} />
+            <Route path='/new/question' element={<AddNewQuestion />} />
+            <Route path='question/:id' element={<Question />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
     </div>
   );
 }
