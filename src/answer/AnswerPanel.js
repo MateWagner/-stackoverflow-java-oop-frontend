b10107f38e7deebae79a1265f262653b04e81e46
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-03-31 06:11:56 +0200

    Reload when posted new question

--- src/answer/AnswerPanel.js
+++ src/answer/AnswerPanel.js
@@ -4,25 +4,23 @@ import { useState } from 'react';
 import useFetchPost from '../api/useFetchPost';
 import { useContext } from 'react';
 import { UserContext } from '../UserContext';
+import { useNavigate } from "react-router-dom";
 const AnswerPanel = ({questionId}) => {
-  const { user } = useContext(UserContext)
-  const [answerContent, setAnswerContent] = useState('');
-  const contentChange = event => { setAnswerContent(event.target.value); }
-  //TODO: Render changes on site without reloading the page.
-  /*  
-  Use the returned ID of the newly posted answer to extend the AnswerList, so
-  we don't have to reload the site for seeing changes.
-  */
- const { postAsync } = useFetchPost();
-  const postNewAnswer = async () => {
-    const url = 'api/answers/';
-    const answerObject = {
-      'desc': answerContent,
-      'questionId': questionId,
-      'clientId': (user != null ? user.id : 1)
-    }
-    const returnedIdOfAnswer = await postAsync(url, answerObject);
-    console.log(returnedIdOfAnswer);
+    const navigate = useNavigate()
+    const { user } = useContext(UserContext)
+    const [answerContent, setAnswerContent] = useState('');
+    const contentChange = event => { setAnswerContent(event.target.value); }
+    
+    const { postAsync } = useFetchPost();
+    const postNewAnswer = async () => {
+      const url = 'api/answers/';
+      const answerObject = {
+        'desc': answerContent,
+        'questionId': questionId,
+        'clientId': (user != null ? user.id : 1)
+      }
+      const returnedIdOfAnswer = await postAsync(url, answerObject);
+      window.location.reload();
   }
   return (
     <div className='answerPanel'>
